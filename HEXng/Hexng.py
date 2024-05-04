9797d9df602b26e0ea9b89bb3d8b7f7692701404
Makar <krikazon@mail.ru>
2024-05-05 04:50:16 +0700

    Hexng: code cleanup

--- HEXng/Hexng.py
+++ HEXng/Hexng.py
@@ -5,6 +5,11 @@ Created on Tue Dec  8 21:26:19 2020
 """
 
 
+# +
+# + [!] needs refactoring. At the moment it's just a mishmash of code
+# + 
+
+
 import math
 import os
 import sys
@@ -12,16 +17,16 @@ import sys
 import png
 from tqdm import trange
 
-from Common.force_input import force_input
-from Common.Timer import Timer
-from TermUtils.term import *
+from src.Common.force_input import force_input
+from src.Common.Timer import Timer
+from src.TermUtils import *
 
 
 APP = os.path.dirname(sys.argv[0])
-info_format = Format(fg=FG.BLUE)
-error_format = Format(fg=FG.YEL, bg=BG.RED, style=STYLE.BOLD)
-ok_format = Format(fg=FG.GREEN, style=STYLE.ITALIC)
-inverted_format = Format(style=STYLE.REVERSE)
+F_INFO = Format(fg=FG.BLUE)
+F_ERROR = Format(fg=FG.YEL, bg=BG.RED, style=STYLE.BOLD)
+F_OK = Format(fg=FG.GREEN, style=STYLE.ITALIC)
+F_INVERTED = Format(style=STYLE.REVERSE)
 
 
 def main(): 
@@ -31,38 +36,41 @@ def main():
     input_files = os.listdir('./in')
     
     if len(input_files) == 0:
-        writef(":: No input files! ::", error_format)
+        writef(":: No input files! ::", F_ERROR)
         write()
-        input("Press Enter to exit...")
         Scr.reset_mode()
         sys.exit(1)
     
-    write(f"number of columns ")
+    write(f"{ffg('?', FG.GREEN)} Number of columns ")
     columns = force_input(ffg(">>> ", FG.GREEN), 
-                          ffg("Value should be larger than 100", FG.RED),
+                          ffg("[ Value should be larger than 100 ]", FGRGB(64, 64, 64)),
                           func=int,
                           predicates=[lambda x: x > 100])
-    write()
+    Scr.clear_os()
     
-    writef("< FILES >", inverted_format)
+    writef("< FILES >", F_INVERTED)
     write()
     [write(f"\t> {file}\n") for file in input_files]
     write()
+        
+    # +
+    # + [!] not a RAM friendly way, but pretty straightforward
+    # +     
 
     with Timer(fstyle("Total", STYLE.BOLD)):
         for file in input_files:
             with open(f'./in/{file}', 'rb') as file_bin:
                 data_hex = file_bin.read().hex()
-                
-            # append missing bytes so we can form a pixel from bytes
-            # every pixel requres 6 bytes (RRGGBB)
+            
+            # append missing bytes so it can form a pixel from bytes
+            # every pixel requires 6 bytes - (0x__ 0x__ 0x__)
             missing_bytes = len(data_hex) % 6
             data_hex += "0" * (6 - missing_bytes)
             
             pixels_amount = len(data_hex) // 6    
             rows = math.ceil(pixels_amount / columns)
             
-            # same action as above but now we must be sure that we can form a row of pixels
+            # append missing pixels so it can form a pixel from
             missing_pixels = rows * columns - pixels_amount
             data_hex += "000000" * missing_pixels
             pixels_amount += missing_pixels
@@ -70,25 +78,31 @@ def main():
             
             write(f"{fstyle(':: filename:', STYLE.BOLD)} {ffg(file, FG.MAGNT)}\n")
             write(f"\tmissing_bytes: {ffg(missing_bytes, FG.RED)}\n")
-            write(f"\trows x columns: {ffg(rows, FG.BLUE)}x{ffg(columns, FG.BLUE)}\n")
-            write(f"\tapproximate pixels amount: {ffg(f'{pixels_amount}px', FG.GREEN)}\n")
+            write(f"\tsize: {ffg(rows, FG.BLUE)}x{ffg(columns, FG.BLUE)}\n")
+            write(f"\tpixels amount: {ffg(f'{pixels_amount}px', FG.GREEN)}\n")
             
             brush = png.Writer(columns, rows, greyscale=False)
                     
             with Timer(fstyle(file, STYLE.BOLD)):            
                 canvas = [0] * triplets_amount
                 for t in trange(triplets_amount, ascii=" -=#", desc=file,
-                                bar_format='{desc}: [{bar:20}] {percentage:.0f}%'):
+                                bar_format='{desc}: [{bar:10}] {percentage:.0f}%'):
                     canvas[t] = int(data_hex[t*2 : t*2+2], 16) 
+                
+                Cur.prev_line()
+                Scr.clear_line()
                     
-                write("Saving...\n")
+                write(ffg("( Saving )\n", FGRGB(64, 64, 64)))
                     
                 with open(f'./out/{file}.png', 'wb') as png_out:
                     brush.write_array(png_out, canvas)
+                    
+                Cur.prev_line()
+                Scr.clear_line()
+            
+            write()
         
-    set_format(ok_format)
-    write("Done!\n")
-    input("Press Enter to exit...")
+    writef("( Done! )\n", F_OK)
                 
                       
 if __name__ == "__main__":
---
+++ HEXng/src/TermUtils/__init__.py
@@ -0,0 +1,8 @@
+from .__term_utils import FG, BG, FGRGB, BGRGB, STYLE, val, fbg, ffg, fstyle
+from .__term_utils import Scr
+from .__term_utils import Cur
+from .__term_utils import Format
+from .__term_utils import Sprite, Animation
+from .__term_utils import set_color, set_style, set_format
+from .__term_utils import write, iwrite, writef, writew, writebox
+from .__term_utils import drawline, drawbox, textbox
