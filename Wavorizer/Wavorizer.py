8910c56b25df0b2ce51d8e1cc910a12f5e83d6b2
Makar <krikazon@mail.ru>
2024-05-08 23:18:01 +0700

    Wavorizer: config system revised, legacy scipy istft no longer used, edge detection mode added, general cleanup

--- Wavorizer/Wavorizer.py
+++ Wavorizer/Wavorizer.py
@@ -7,24 +7,46 @@ Created on Tue Jun 23 21:07:56 2020
 
 # +
 # + [!] needs refactoring. At the moment it's just a mishmash of code
-# + 
+# +
 
 
 import io
 import math
 import os
 import sys
+import re
 from typing import Union
+from enum import StrEnum
 
 import numpy as np
 from PIL import Image
 from scipy.io.wavfile import write as wav_write
-from scipy.signal import istft
+from scipy.signal import ShortTimeFFT
+from scipy.ndimage import sobel
+from cerberus import Validator, SchemaError
 
 from src.Common.force_input import force_input
 from src.Common.JSONConfig import JSONConfig
 from src.Common.Timer import Timer
 from src.TermUtils import *
+
+
+Number = Union[int, float]
+
+
+class Mode(StrEnum):
+    ISM = "ISM"
+    PBP = "PBP"
+    
+    
+class SampleRateMode(StrEnum):
+    STATIC = "static"
+    DYNAMIC = "dynamic"
+    
+    
+class Direction(StrEnum):
+    ROWS = "rows"
+    COLS = "cols"
 
 
 CFG_NAME = "config.json"
@@ -34,8 +56,71 @@ F_INFO = Format(fg=FG.BLUE)
 F_ERROR = Format(fg=FG.YEL, bg=BG.RED, style=STYLE.BOLD)
 F_OK = Format(fg=FG.GREEN, style=STYLE.ITALIC)
 F_INVERTED = Format(style=STYLE.REVERSE)
+C_GRAY = FGRGB(64, 64, 64)
 
-Number = Union[int, float]
+
+MODES = (Mode.ISM, Mode.PBP)
+DIRECTIONS = (Direction.ROWS, Direction.COLS)
+SAMPLERATE_MODES = (SampleRateMode.STATIC, SampleRateMode.DYNAMIC)
+SAMPLERATE_RANGE = range(512, 256000 + 1)
+IMG_SCALE_RANGE = range(1, 10 + 1)
+
+
+CFG_VALIDATOR = Validator(
+    {
+        "mode": {"type": "string", "allowed": MODES},
+        "channels": {"type": "integer", "allowed": [1, 2]},
+        "sample_rate_mode": {"type": "string", "allowed": SAMPLERATE_MODES},
+        "sample_rate": {
+            "type": "integer",
+            "min": SAMPLERATE_RANGE[0],
+            "max": SAMPLERATE_RANGE[-1],
+        },
+        "image_scale": {
+            "type": "integer",
+            "min": IMG_SCALE_RANGE[0],
+            "max": IMG_SCALE_RANGE[-1],
+        },
+        "PBP": {
+            "type": "dict",
+            "schema": {
+                "sum_rgb_components": {"type": "boolean"},
+                "direction": {
+                    "type": "string",
+                    "allowed": DIRECTIONS,
+                },
+            },
+        },
+        "ISM": {
+            "type": "dict",
+            "schema": {
+                "use_noise": {"type": "boolean"},
+                "noise_strength": {"type": "float", "min": 0.0, "max": 1.0},
+                "detect_edges": {"type": "boolean"},
+            }
+        },
+    }, require_all=True
+)
+
+
+CFG_DEFAULT = JSONConfig(
+    {
+        "mode": Mode.PBP,
+        "channels": 1,
+        "sample_rate_mode": SampleRateMode.STATIC,
+        "sample_rate": 44100,
+        "image_scale": 1,
+        "PBP": {
+            "sum_rgb_components": False,
+            "direction": Direction.ROWS
+        },
+        "ISM": {
+            "use_noise": True,
+            "noise_strength": 0.5,
+            "detect_edges": False
+        }
+    }, True
+)
 
 
 def pbp(image: Image, linear_size: int,
@@ -56,14 +141,15 @@ def pbp(image: Image, linear_size: int,
     if use_sum_rgb:
         # sum rgb components into a single value
         image_data = image_data.reshape(linear_size, 3)
-        image_data = np.sum(image_data, 1)
-
+        image_data = np.sum(image_data, 1, np.uint8)
+    
     # return as 1dim array of audio data
     return image_data.flatten()
 
 
 def ism(image: Image,
-        use_noise: bool, noise_strength: float):
+        use_noise: bool, noise_strength: float,
+        detect_edges: bool):
     # +
     # + ISM - inverse spectrogram method of image decoding into sound data
     # +
@@ -72,11 +158,20 @@ def ism(image: Image,
     image_data = np.array(image)[::-1] / 255
     
     if use_noise:
-        noise = np.abs(np.random.normal(
-            0, 1, image_data.shape)) * noise_strength
+        noise = np.abs(np.random.normal(0, 1, 
+                                        image_data.shape)) * noise_strength
         image_data *= 1 - noise
 
-    return istft(image_data, scaling="psd")[1]
+    if detect_edges:
+        edges_x = sobel(image_data, 0)
+        edges_y = sobel(image_data, 1)
+        image_data = np.hypot(edges_x, edges_y)
+        
+    stfft = ShortTimeFFT.from_window(('cosine'), 1, 
+                                     image_data.shape[0] * 2 - 1, 
+                                     image_data.shape[0])
+
+    return stfft.istft(image_data)
 
 
 def clamp(value: Number,
@@ -118,92 +213,60 @@ def clamped_linear_remap(value: Number,
 
 
 def main():
-    modes = ("ISM", "PBP")
-    directions = ("row", "column")
-    sample_rate_range = range(512, 256001)
-    image_scale_range = range(1, 11)
-    
-    default_config = JSONConfig(
-        {
-            "mode": "PBP",
-            "channels": 1,
-            "sample_rate_locked": True,
-            "sample_rate": 44100,
-            "image_scale": 1,
-            "PBP": {
-                "sum_rgb_components": False,
-                "direction": "row"
-            },
-            "ISM": {
-                "use_noise": True,
-                "noise_strength": 0.5
-            }
-        }
-    )
-    
     os.chdir(APP)
     os.makedirs("in", exist_ok=True)
     os.makedirs("out", exist_ok=True)
 
     try:
-        config = JSONConfig.load(CFG_NAME)
-        if config.schema != default_config.schema:
-            raise KeyError
+        config = JSONConfig.load(CFG_NAME, True)
 
-    except (FileNotFoundError, KeyError, JSONConfig.FileCorruptedError):
-        write(f"config.json does not exist or corrupted, create new? [y/n] ")
-        yn = force_input(ffg('>>> ', FG.GREEN), "[y/n]",
-                         func=str.lower,
-                         predicates=[lambda x: x in ('y', 'n')])
-        if yn == 'y':
-            default_config.save(CFG_NAME)
-        Scr.reset_mode()
-        sys.exit(1)
-
-    mode = config.get("mode")
-    channels = config.get("channels")
-    sample_rate_locked = config.get("sample_rate_locked")
-    sample_rate = config.get("sample_rate")
-    image_scale = config.get("image_scale")
-    pbp_sum_rgb_components = config.section("PBP").get("sum_rgb_components")
-    pbp_direction = config.section("PBP").get("direction")
-    ism_use_noise = config.section("ISM").get("use_noise")
-    ism_noise_strength = config.section("ISM").get("noise_strength")
+    except FileNotFoundError:
+        CFG_DEFAULT.save(CFG_NAME)
+        config = JSONConfig(CFG_DEFAULT.as_dict(), True)
+        writef("( Config file created )", F_INFO)
+        write("\n\n")
     
+    except OSError:
+        writef(":: Unknown OS error ::", F_ERROR)
+        write()
+        Scr.reset_mode()
+        sys.exit(1)
+        
     try:
-        if mode not in modes:
-            raise ValueError(
-                f"modes available: [PBP, ISM]; current: {mode}")
-        if channels not in (1, 2):
-            raise ValueError(
-                f"channels available: [1 - (Mono), 2 - (Stereo)]; current: {channels}")
-        if pbp_direction not in directions:
-            raise ValueError(
-                f"directions available: [row, column]; current: {pbp_direction}")
-
-        if sample_rate not in sample_rate_range:
-            raise ValueError(
-                f"sample rate must be in range({sample_rate_range[0]} ... {sample_rate_range[-1]}); current: {sample_rate}")
-        if image_scale not in image_scale_range:
-            raise ValueError(
-                f"image scale must be in range({image_scale_range[0]} ... {image_scale_range[-1]}); current: {image_scale}")
-        if not (0.0 <= ism_noise_strength <= 1.0):
-            raise ValueError(
-                f"noise strength must be a factor in range(0.0 ... 1.0); current: {ism_noise_strength}")
-
-    except ValueError as e:
-        write(f"Config validation failed: {ffg(f'[!] {e}', FG.RED)} \n")
-        write("Fix it yourself or reset\n")
-        write("Reset? [y/n] ")
-
-        yn = force_input(ffg('>>> ', FG.GREEN), "[y/n]",
+        if not CFG_VALIDATOR.validate(config.as_dict()):
+            raise SchemaError
+    
+    except SchemaError:
+        write("Config validation failed:\n")
+        
+        for k, v in CFG_VALIDATOR.errors.items():
+            reason = re.sub(r"[\[\]\{\}']", "", str(v))
+            err_str = f"\t{ffg('>', FG.RED)} {k:<30} : {ffg(reason, FG.RED)}"
+            print(err_str)
+        
+        write()
+        
+        write(f"Reset {CFG_NAME}?\n")
+        write(f"{ffg('( [y/n] )', C_GRAY)}\n")
+        yn = force_input(ffg(">>> ", FG.GREEN), ffg("( [y/n] )", C_GRAY),
                          func=str.lower,
                          predicates=[lambda x: x in ('y', 'n')])
         if yn == 'y':
-            default_config.save(CFG_NAME)
+            CFG_DEFAULT.save(CFG_NAME)
 
         Scr.reset_mode()
         sys.exit(1)
+
+    mode = config.mode
+    channels = config.channels
+    sample_rate_mode = config.sample_rate_mode
+    sample_rate = config.sample_rate
+    image_scale = config.image_scale
+    pbp_sum_rgb_components = config.PBP.sum_rgb_components
+    pbp_direction = config.PBP.direction
+    ism_use_noise = config.ISM.use_noise
+    ism_noise_strength = config.ISM.noise_strength
+    ism_detect_edges = config.ISM.detect_edges
 
     input_files = set(os.listdir('./in'))
     img_files = set(filter(lambda f:
@@ -215,60 +278,76 @@ def main():
         write()
         Scr.reset_mode()
         sys.exit(1)
-        
+
     writef("< FILES >", F_INVERTED)
     write()
     [write(f"\t> {file}\n") for file in img_files]
     [write(ffg(f"\t- {file}\n", FG.RED)) for file in other_files]
-    
+
     write()
-    
+
     write("Mode: ")
     write(fstyle(ffg(mode, FG.YEL), STYLE.UNDER))
     write()
-
-    write("Dynamic sample rate: ")
-    if not sample_rate_locked:
-        write(ffg("+", FG.GREEN))
-    else:
-        write(ffg("-", FG.RED))
-        write()
-        write(f"Static sample rate: {ffg(sample_rate, FG.CYAN)}")
-    write()
-        
-    write("Channels: ")
-    write(ffg(channels, FG.GREEN))
-    write()
     
-    write("Upscale: ")
+    write(" + Sample rate mode: ")
+    write(fstyle(ffg(sample_rate_mode, FG.CYAN), STYLE.UNDER))
+    write()
+
+    if sample_rate_mode == SampleRateMode.STATIC:
+        write(f" └ Sample rate: {ffg(sample_rate, FG.CYAN)}")
+    else:
+        write(f" └ Sample rate: {ffg('~', C_GRAY)}")
+    write()
+
+    write("Channels: ")
+    write(ffg(channels, FG.MAGNT))
+    write()
+
+    write(" + Upscale: ")
     if image_scale != 1:
         write(ffg("+", FG.GREEN))
-        write()
-        write(f"Scale factor: {ffg(image_scale, FG.BLUE)}")
     else:
         write(ffg("-", FG.RED))
     write()
     
-    if mode == "PBP":
-        write("[+] PBP\n")
-        write(f"\tdirection: {ffg(pbp_direction, FG.YEL)}\n")
+    write(f" └ Scale factor: {ffg(image_scale, FG.BLUE)}\n")
+
+    write()
+
+    if mode == Mode.PBP:
+        write(f"{ffg('[*]', C_GRAY)} {fstyle(Mode.PBP, STYLE.BOLD)}:\n")
+        write(f"\tDirection: {ffg(pbp_direction, FG.YEL)}\n")
         if pbp_sum_rgb_components:
-            write(f"\tuse sum of {ffg('r', FG.RED)}+{ffg('g', FG.GREEN)}+{ffg('b', FG.BLUE)} components")
+            write(
+                f"\tUse sum of {ffg('r', FG.RED)}{ffg('+', C_GRAY)}{ffg('g', FG.GREEN)}{ffg('+', C_GRAY)}{ffg('b', FG.BLUE)} components"
+                )
         else:
-            write(f"\tuse {ffg('r', FG.RED)}{ffg('g', FG.GREEN)}{ffg('b', FG.BLUE)} components separately")
-    elif mode == "ISM":
-        write("[+] ISM\n")
+            write(f"\tUse {ffg('r', FG.RED)}{ffg('g', FG.GREEN)}{ffg('b', FG.BLUE)} components separately")
+
+    elif mode == Mode.ISM:
+        write(f"{ffg('[*]', C_GRAY)} {fstyle(Mode.ISM, STYLE.BOLD)}:\n")
+        write(f"\t + Apply noise: ")
         if ism_use_noise:
-            write(f"\tapply noise with strength: {ffg(ism_noise_strength, FG.YEL)}")
+            write(ffg("+", FG.GREEN))
         else:
-            write("...")
-    write()
-                
-    write()
+            write(ffg("-", FG.RED))
+        write()
         
+        write(f"\t └ Noise strength: {ffg(ism_noise_strength, FG.YEL)}\n")
+        
+        write("\tEdge detection mode: ")
+        if ism_detect_edges:
+            write(ffg("on", FG.GREEN))
+        else:
+            write(ffg("off", FG.RED))
+        write()
+
+    write()
+
     with Timer(fstyle("Total", STYLE.BOLD)):
         for file in img_files:
-            write(f"{fstyle(':: filename:', STYLE.BOLD)} {ffg(file, FG.MAGNT)}\n")
+            write(f"{fstyle(':: File:', STYLE.BOLD)} {ffg(file, FG.MAGNT)}\n")
 
             try:
                 with open(f"./in/{file}", 'rb') as file_img:
@@ -279,86 +358,76 @@ def main():
                 write()
                 write(f"{ffg('>> SKIP', FG.RED)}\n")
                 continue
-                 
-                
+
             if image_scale != 1:
                 orig_size = image.size
                 image = image.resize((round(image.size[0] * image_scale),
                                       round(image.size[1] * image_scale)),
                                      resample=Image.NEAREST)
-                
-                write("\t+ image resized:\n")
-                write(f"\t├ scale: {ffg(image_scale, FG.BLUE)}\n")
-                write(f"\t{f'└ original size:':<30} {ffg(orig_size[0], FG.BLUE)}x{ffg(orig_size[1], FG.BLUE)}\n")
-                
+
+                writef("\t( Image resized )", F_INFO)
+                write()
+                write(f"\t + Scale: {ffg(image_scale, FG.BLUE)}\n")
+                write(f"\t └ Original size: {ffg(orig_size[0], FG.BLUE)}x{ffg(orig_size[1], FG.BLUE)}\n")
+
             width, height = image.size
             image_lin_size = width * height
-            
-            write(f"\t{f'size:':<30} {ffg(width, FG.BLUE)}x{ffg(height, FG.BLUE)}\n")
-            write(f"\t{f'pixels amount:':<30} {ffg(image_lin_size, FG.CYAN)}px\n")
-                        
+
+            write(f"\tSize: {ffg(width, FG.BLUE)}x{ffg(height, FG.BLUE)}\n")
+            write(f"\tPixels amount: {ffg(image_lin_size, FG.YEL)}px\n")
+
             if image_lin_size > MAX_IMAGE_LIN_SIZE:
                 writef("\t:: Image is too large ::", F_ERROR)
                 write()
                 write(f"{ffg('>> SKIP', FG.RED)}\n")
                 continue
             
-            if not sample_rate_locked:
+            if mode == Mode.ISM and (width < 64 or height < 64):
+                writef(
+                    f"\t:: ISM mode requires width\height to be 64px+ ::", F_ERROR)
+                write()
+                write(f"{ffg('>> SKIP', FG.RED)}\n")
+                continue
+
+            if sample_rate_mode == SampleRateMode.DYNAMIC:
                 sample_rate = round(clamped_linear_remap(image_lin_size,
                                                     1, MAX_IMAGE_LIN_SIZE,
-                                                    sample_rate_range[0], sample_rate_range[-1]))
-                write(f"\tsample rate: {ffg(sample_rate, FG.YEL)}\n")
+                                                    SAMPLERATE_RANGE[0], SAMPLERATE_RANGE[-1]))
+                write(f"\tSample rate: {ffg(sample_rate, FG.CYAN)}\n")
 
             audio_data = None
-            
+
             with Timer(fstyle(file, STYLE.BOLD)):
+                write(ffg("( Processing )\n", C_GRAY))
+                
                 if mode == "PBP":
-                    write(ffg("( Processing )\n", FGRGB(64, 64, 64)))
-                    
                     audio_data = pbp(image, image_lin_size,
                                      pbp_direction, pbp_sum_rgb_components)
-                    Cur.prev_line()
-                    Scr.clear_line()
 
                 elif mode == "ISM":
-                    if width < 64 or height < 64:
-                        writef(
-                            f"\t:: ISM mode requires width\height to be {ffg('64px+', FG.RED)} ::", F_ERROR)
-                        write()
-                        write(f"{ffg('>> SKIP', FG.RED)}\n")
-                        continue
+                    audio_data = ism(image, ism_use_noise, ism_noise_strength,
+                                     ism_detect_edges)
 
-                    if width != height:
-                        width = round(width * (height / width))
-                        image = image.resize(
-                            (width, height), resample=Image.LANCZOS
-                        )
-                        write(f"\t* aspect ratio {ffg('fixed', FG.GREEN)}\n")
-
-                    write(ffg("( Processing )\n", FGRGB(64, 64, 64)))
-
-                    audio_data = ism(image, ism_use_noise, ism_noise_strength)
-                    
-                    Cur.prev_line()
-                    Scr.clear_line()
+                Cur.prev_line()
+                Scr.clear_line()
 
                 if channels == 2:
                     if audio_data.shape[0] % 2 == 1:
                         audio_data = np.append(audio_data, 0)
-                    audio_data = audio_data.reshape((len(audio_data)//2, 2))
+                    audio_data = audio_data.reshape((len(audio_data) // 2, 2))
 
-                write(ffg("( Saving )\n", FGRGB(64, 64, 64)))
-                
+                write(ffg("( Saving )\n", C_GRAY))
+
                 wav_write(f"./out/{file}_{mode}_{sample_rate}HZ.wav", sample_rate, audio_data)
-                
+
                 Cur.prev_line()
                 Scr.clear_line()
-                
+
             write()
-            
+
     writef("( Done! )\n", F_OK)
 
-    
+
 if __name__ == '__main__':
     Scr.color_on()
     try:
--- Wavorizer/src/Common/JSONConfig.py
+++ Wavorizer/src/Common/JSONConfig.py
@@ -1,96 +1,114 @@
 import json
+from typing import Any, List
+
+
+class JSONConfigError(Exception):
+    pass
+
+
+class InvalidFileContent(JSONConfigError):
+    pass
+
 
 class JSONConfig:
-    class FileCorruptedError(Exception):
-        ...
+    __read_only: List
+
+    def __setattr__(self, name: str, value: Any):
+        if name in self.__read_only:
+            raise AttributeError(f"Attribute {self.__class__.__qualname__}.{name} is read-only")
+        
+        self.__dict__[name] = value
     
-    def __init__(self, default_config: dict = {}):
-        self.config = default_config
-        self.schema = self._get_schema(default_config)
+    def __init__(self, config: dict, attributes_mode: bool = False):
+        self.__dict__[f"_{self.__class__.__name__}__read_only"] = []    
         
-                
-    def _get_schema(self, base_dict: dict, recursion_level: int = 4):
-        schema = dict.fromkeys(base_dict.keys())
+        self.__config = config.copy()
+        self.__attributes_mode = attributes_mode
+
+        self.__preprocess()    
+        self.__schema = self.__generate_schema()
         
-        if recursion_level != 0:
-            for key in schema:
-                elem = base_dict[key]
-                schema[key] = type(elem)
-                if isinstance(elem, dict):
-                    schema[key] = self._get_schema(elem, recursion_level-1)
-                    
+        if attributes_mode:
+            self.__generate_attributes()
+
+    @property
+    def schema(self):
+        return self.__schema.copy()
+    
+    @property
+    def attribute_mode(self):
+        return self.__attributes_mode
+    
+    def __preprocess(self):
+        for key, val in self.__config.items():
+            if isinstance(val, dict):
+                self.__config[key] = JSONConfig(val, self.__attributes_mode)
+    
+    def __generate_schema(self):
+        schema = dict.fromkeys(self.__config.keys())
+        
+        for key, val in self.__config.items():
+            if isinstance(val, JSONConfig):
+                schema[key] = val.schema
+            else:
+                schema[key] = type(val)
+    
         return schema
     
-    
-    def _get_exc_none(exc: Exception):
-        def layer(func):
-            def wrapper(self, *args):
-                try:
-                    return func(self, *args)
-                except exc:
-                    return None
-                
-            return wrapper
-        
-        return layer
-       
-        
-    def get(self, key: str):
-        """
-        @returns: the value as is or raises KeyError
-        """
-        return self.config[key]
-    
-    
-    def get_or_none(self, key: str):
-        """
-        @returns: value as is or None
-        """        
-        return self.config.get(key, None)
-    
-    
-    def get_int(self, key: str):
-        """
-        @returns: value as int
-        """
-        return int(self.get(key))
-    
-    
-    def get_bool(self, key: str):
-        """
-        @returns: value as bool
-        """
-        return bool(self.get(key))
-    
-    
-    def get_float(self, key: str):
-        """
-        @returns: value as float
-        """
-        return float(self.get(key))
-    
-    
-    def section(self, section_name: str):
-        if isinstance(sect := self.get(section_name), dict):
-            return JSONConfig(sect)
-        else: 
-            raise ValueError("Not a section")
-        
+    def __generate_attributes(self):
+        for key, val in self.__config.items():
+            key = str(key)
             
+            if not key.isidentifier():
+                raise SyntaxError(f"'{key}' is not a valid python identifier")
+            
+            if hasattr(self, key):
+                raise AttributeError(f"Attribute {self.__class__.__qualname__}.{key} already exists")
+            
+            setattr(self, key, val)
+
+            self.__read_only.append(key)
+        
+    def get(self, key: str, default=None):
+        """
+        Returns:
+            value or default
+        """        
+        return self.__config.get(key, default)
+          
     @classmethod
-    def load(cls, file_path: str) -> 'JSONConfig':
+    def load(cls, file_path: str, attributes_mode: bool = False) -> 'JSONConfig':
+        """
+        Returns:
+            JSONConfig with content from file, if .json file is invalid,
+            empty JSONConfig will be returned
+        """
         try:
             with open(file_path, 'r', encoding='utf8') as config_file:
                 json_data = json.load(config_file)
         except json.JSONDecodeError:
-            raise JSONConfig.FileCorruptedError
+            json_data = {}
             
-        return JSONConfig(json_data)
+        return JSONConfig(json_data, attributes_mode)
     
     def save(self, file_path: str):
         with open(file_path, 'w', encoding='utf-8') as config_file:
-            config_file.write(json.dumps(self.config, indent=4)) 
+            config_file.write(json.dumps(self.as_dict(), indent=4)) 
             
+    def as_dict(self):
+        dictionary = self.__config.copy()
+        sections = filter(lambda e: isinstance(e[1], JSONConfig), dictionary.items())
     
-    def __str__ (self) -> str:
-        return str(self.config)
+        for key, val in sections:
+            dictionary[key] = val.as_dict()
+            
+        return dictionary
+        
+    def __getitem__(self, key):
+        return self.__config[key]
+            
+    def __str__ (self):
+        return str(self.as_dict())
+    
+    def __repr__(self):
+        return f"{self.__class__.__qualname__}({self.as_dict()}, {self.__attributes_mode})"
